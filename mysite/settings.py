a4dc4f6eb399c1a3bf2a92bfc171289343240585
Fahim Ahmed Shakil <fahimahmed38@gmail.com>
2019-04-13 16:06:01 +0600

    added app url to allowed host in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '!4#q5!&k15_f#qe_kz-afv#qye_k+tekg1f2$%fxh6+a7l0y^8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sampledjangoblog.herokuapp.com',]
 
 
 # Application definition
