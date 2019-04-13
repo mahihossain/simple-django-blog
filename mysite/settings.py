dd2a4bebf0189657ef144a394568305b72dc7bbe
Fahim Ahmed Shakil <fahimahmed38@gmail.com>
2019-04-13 15:50:21 +0600

    fixed alias for whitenoise in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'mysite.urls'
 
