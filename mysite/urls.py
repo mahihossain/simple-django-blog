94ad18d7de31fa2889fa0ac6df63b32424359dea
mahihossain <sayed.muddashir@gmail.com>
2019-04-14 21:04:33 +0600

    Minor Changes.

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,11 +20,11 @@ from django.urls import path, include
 from blog.views.register import RegisterView
 
 urlpatterns = [
-    path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='blog:home')),
     path('register/', RegisterView.as_view(), name='register'),
     path('', include('django.contrib.auth.urls'))
-    # path('', include('blog.urls'))
+    # path('blog/', include('blog.urls'))
 ]
